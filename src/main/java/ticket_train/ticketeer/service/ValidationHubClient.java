82636cf373970c28399b06758227dd06a889aaa5
melisdamla <melissdamlasahin@gmail.com>
2026-04-26 19:34:45 +0200

    Project created

--- src/main/java/ticket_train/ticketeer/service/ValidationHubClient.java
+++
@@ -1,8 +0,0 @@
-package ticket_train.ticketeer.service;
-
-import ticket_train.ticketeer.model.Billet;
-import ticket_train.ticketeer.model.SegmentBillet;
-
-public interface ValidationHubClient {
-    void confirmValidation(Billet billet, SegmentBillet segment);
-}
